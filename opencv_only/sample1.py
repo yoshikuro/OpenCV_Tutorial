93b0555b74657111ea54c6fa1556800713def685
yoshikuro <44320012+yoshikuro@users.noreply.github.com>
2022-04-26 21:29:51 +0900

    Update sample1.py

--- opencv_only/sample1.py
+++ opencv_only/sample1.py
@@ -3,6 +3,6 @@ import cv2
 
 img = cv2.imread("img/OpenCV.png")	# 画像を変数imgに代入
 
-cv2.imshow("OpenCV", img)			# 名前が「OpenCV」のウィンドウにimgを表示
-cv2.waitKey(0)						# キー入力待ち状態を示す/入力されるキーは指定なし
-cv2.destroyAllWindows()				# すべてのウィンドウを閉じる
+cv2.imshow("OpenCV", img)		# 名前が「OpenCV」のウィンドウにimgを表示
+cv2.waitKey(0)				# キー入力待ち状態を示す/入力されるキーは指定なし
+cv2.destroyAllWindows()			# すべてのウィンドウを閉じる
